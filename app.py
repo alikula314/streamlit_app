ab0cef3a3cb58756596c602fa6a842e39ade8bc6
Muhammet Ali Kula <63548349+alikula314@users.noreply.github.com>
2022-01-18 15:08:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from sklearn import datasets
+pip install -U scikit-learn scipy matplotlib
 import pickle
 import pandas as pd
 import numpy as np
+import sklearn
 
 st.set_page_config(page_title="Football Player Wage Prediction", page_icon=":bank:", layout="wide")
 
